53e57a44e4a2fd1fdb8b11dfb527087145ccfc85
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-08-13 12:06:52 +0200

    Changed the device controller

--- src/js/controllers/device_controller.js
+++ src/js/controllers/device_controller.js
@@ -7,25 +7,47 @@ define(function () {
 
 	var checkUseragent = function() {
 		return navigator.userAgent;
-		console.log(navigator.userAgent);
 	}
 
 	return {
 		deviceType : function() {
-			if(checkUseragent().match(/Android/i)) {
-				return 'Android';
+			if(checkUseragent().match(/Android/i) || 
+				checkUseragent().match(/webOS/i) || 
+				checkUseragent().match(/iPhone/i) ||
+				checkUseragent().match(/iPod/i) ||
+				checkUseragent().match(/iPad/i)){
+				return 'mobile';
+			} else {
+				return 'desktop';
 			}
-			if(checkUseragent().match(/webOS/i)) {
-				return 'webOS';
-			}
-			if(checkUseragent().match(/iPhone/i)) {
-				return 'iPhone';
-			}
-			if(checkUseragent().match(/iPod/i)) {
-				return 'iPod';
-			}
-			if(checkUseragent().match(/iPad/i)) {
-				return 'iPad';
+		},
+		deviceOS : function() {
+			if(deviceType().match(/mobile/i)) {
+				if(checkUseragent().match(/Android/i)) {
+					return 'Android';
+				}
+				if(checkUseragent().match(/webOS/i)) {
+					return 'webOS';
+				}
+				if(checkUseragent().match(/iPhone/i)) {
+					return 'iPhone';
+				}
+				if(checkUseragent().match(/iPod/i)) {
+					return 'iPod';
+				}
+				if(checkUseragent().match(/iPad/i)) {
+					return 'iPad';
+				}
+			} else {
+				if(checkUseragent().match(/Windows NT/i)) {
+					return 'Windows';
+				}
+				if(checkUseragent().match(/Mac OS/i)) {
+					return 'Mac';
+				}
+				if(checkUseragent().match(/Linux/i)) {
+					return 'Mac';
+				}
 			}
 		}
 	}
